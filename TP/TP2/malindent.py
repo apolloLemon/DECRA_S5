f7fe7b75e0f4d41e4e1aab3c9b5213af36e15cf6
Matthew Coyle <matthew@coyle.fr>
2019-11-08 10:08:31 +0100

    TP2exo2init

---
+++ TP/TP2/malindent.py
@@ -0,0 +1,21 @@
+# mini mvc en python3
+
+class Model:
+def get_post(self):
+return {"title":"A test","body":"An example.."}
+
+class View:
+def display(self,items):
+print('Title:' + items['title'] + '\n'+'Body:' + items['body'])
+
+class Controller:
+def __init__(self):
+self.model=Model()
+self.view=View()
+
+def main(self):
+post=self.model.get_post()
+self.view.display(post)
+
+mvc=Controller()
+mvc.main()
